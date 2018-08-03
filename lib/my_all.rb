8a11276cd77f545fe88889d81ff778253906b67e
Sav Younts <savyounts@gmail.com>
2018-08-03 20:53:24 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0 
+  block_returns = []
+  while i < colleciton.length 
+  
+    block_returns << yield(collection[i])
+    i += 1 
+  end 
+  
+  if block_return.include?(false)
+    false
+  else
+    true 
+  end 
 end
